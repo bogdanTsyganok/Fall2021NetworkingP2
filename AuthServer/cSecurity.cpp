8d82524361acf6e8098e64ae4cbdbe66544ad34f
Jarrid <JarridSteeper@gmail.com>
2021-11-10 23:13:07 -0500

    security class fleshed out and documentation updated

--- AuthServer/cSecurity.cpp
+++ AuthServer/cSecurity.cpp
@@ -1,18 +1,59 @@
+/*
+* Author:		Jarrid Steeper 0883583, Bogdan Tsyganok 0886354
+* Class:		INFO6016 Network Programming
+* Teacher:		Lukas Gustafson
+* Project:		Project02
+* Due Date:		Nov 12
+* Filename:		cSecurity.h
+* Purpose:		Security class definition for generating a salt and hash for user created password that will be stored in the authentification database.
+*/
+
 #include <string>
 #include "cSecurity.h"
+#include "sha256.h"
 
-std::string cSecurity::GenerateSalt()
+cSecurity::cSecurity()
+{
+	this->mSalt = "";
+}
+
+cSecurity::~cSecurity()
+{
+}
+
+std::string cSecurity::GetSalt()
+{
+	return this->mSalt;
+}
+
+void cSecurity::GenerateSalt()
 {
 	char alphanum[] =
 		"0123456789"
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		"abcdefghijklmnopqrstuvwxyz"
 		"`~,<.>/?;:'\"[{]}-_=+\\|/*";
-	std::string output;
-	output.reserve(64);
+	std::string newSalt;
+	newSalt.reserve(64);
 	for (int i = 0; i < 64; i++)
 	{
-		output += alphanum[rand() % (sizeof(alphanum) - 1)];
+		newSalt += alphanum[rand() % (sizeof(alphanum) - 1)];
 	}
-	return output;
+	this->mSalt = newSalt;
+}
+
+std::string cSecurity::GenerateHash(std::string plainTextPassword)
+{
+	if (this->mSalt == "")
+	{
+		GenerateSalt();
+	}
+	SHA256 sha256;
+	std::string temp = mSalt + plainTextPassword;
+	return sha256(temp);
+}
+
+void cSecurity::Reset()
+{
+	this->mSalt = "";
 }
--- AuthServer/cSecurity.h
+++ AuthServer/cSecurity.h
@@ -1,7 +1,43 @@
+/*
+* Author:		Jarrid Steeper 0883583, Bogdan Tsyganok 0886354
+* Class:		INFO6016 Network Programming
+* Teacher:		Lukas Gustafson
+* Project:		Project02
+* Due Date:		Nov 12
+* Filename:		cSecurity.h
+* Purpose:		Security class declaration for generating a salt and hash for user created password that will be stored in the authentification database.
+*/
+
 #pragma once
 
 class cSecurity
 {
+private:
+	std::string mSalt;
 public:
-	static std::string GenerateSalt();
+	//Constructors and destructors
+	cSecurity();
+	~cSecurity();
+
+	//Getters and setters
+	std::string GetSalt();
+
+	//Methods
+
+	/// <summary>
+	/// Generates a new salt for use in password hashing
+	/// </summary>
+	void GenerateSalt();
+
+	/// <summary>
+	/// Hashes user given plain text password with sha256 and stored salt, if no salt is stored it will generate new salt
+	/// </summary>
+	/// <param name="plainTextPassword"></param>
+	/// <returns></returns>
+	std::string GenerateHash(std::string plainTextPassword);
+
+	/// <summary>
+	/// Resets the stored salt
+	/// </summary>
+	void Reset();
 };
